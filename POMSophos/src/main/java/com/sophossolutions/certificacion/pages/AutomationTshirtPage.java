1f41233ecb29b9072df24b03795cf6b4429a2c4a
Alejandro Araque Marin <alejandro.araque@sophossolutions.com>
2022-10-13 11:10:35 -0500

    solucionado problema en automation practice

--- POMSophos/src/main/java/com/sophossolutions/certificacion/pages/AutomationTshirtPage.java
+++ POMSophos/src/main/java/com/sophossolutions/certificacion/pages/AutomationTshirtPage.java
@@ -4,15 +4,22 @@ import com.sophossolutions.certificacion.actions.Action;
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+
 public class AutomationTshirtPage extends PageObject {
 
 	By tShirtColorOrange = By.xpath("//*[@id='color_1']");
 	By tShirtColorBlue = By.xpath("//*[@id='color_2']");
+	Long duracion = 5L;
 
 	public void clicColor(String color) {
 		if (color.equals("Orange")) {
 			Action.clicTo(getDriver(), tShirtColorOrange);
+			Action.waitForElement(getDriver(), tShirtColorOrange, duracion);
+			Action.clicTo(getDriver(), tShirtColorOrange);
 		} else {
+			Action.clicTo(getDriver(), tShirtColorBlue);
+			Action.waitForElement(getDriver(), tShirtColorBlue, duracion);
 			Action.clicTo(getDriver(), tShirtColorBlue);
 		}
 	}
