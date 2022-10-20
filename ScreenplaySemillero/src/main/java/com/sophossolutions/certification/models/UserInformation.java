2bf23b732096cece6d3837113956a32b181c9d4b
Cristian Stiven Monsalve Ruiz <cristian.monsalve@sophossolutions.com>
2022-10-20 10:03:16 -0500

    creando la interfaz sesion checkout

---
+++ ScreenplaySemillero/src/main/java/com/sophossolutions/certification/models/UserInformation.java
@@ -0,0 +1,18 @@
+package com.sophossolutions.certification.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInformation {
+
+	private String name;
+	private String lastname;
+	private String postalcode;
+
+}
+
+
+
+
---
+++ ScreenplaySemillero/src/main/java/com/sophossolutions/certification/userinfaces/CheckoutInformationPage.java
@@ -0,0 +1,14 @@
+package com.sophossolutions.certification.userinfaces;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class CheckoutInformationPage {
+    public static final Target FIRST_NAME = Target.the("el nombre del usuario").locatedBy("name:firstName");
+
+    public static final Target LAST_NAME = Target.the("el apellido del usuario").locatedBy("name:lastName");
+
+    public  static final Target POSTAL_CODE = Target.the("el apellido del usuario").locatedBy("id:postal-code");
+
+    public static final Target BTN_CONTINUE = Target.the("boton continue").locatedBy("//*[@id=\"continue\"]");
+
+}
